7a052f38f83d1f948ad7038c0ffbfd3b009b96a6
Joey Frohikey <frohikey@gmail.com>
2016-09-29 17:19:29 +0200

    Microsoft.Azure.DocumentDB ref updated.

--- Oogi/Oogi/Properties/AssemblyInfo.cs
+++ Oogi/Oogi/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.265.0908.9")]
-[assembly: AssemblyFileVersion("2016.265.0908.9")]
+[assembly: AssemblyVersion("2016.273.1518.11")]
+[assembly: AssemblyFileVersion("2016.273.1518.11")]
--- Oogi/Tests/Properties/AssemblyInfo.cs
+++ Oogi/Tests/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.265.0923.17")]
-[assembly: AssemblyFileVersion("2016.265.0923.17")]
+[assembly: AssemblyVersion("2016.273.1518.19")]
+[assembly: AssemblyFileVersion("2016.273.1518.19")]
