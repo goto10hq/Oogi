c4ea6eb0d0948453cd2499204c2fecb83ae61b75
Joey Frohikey <frohikey@gmail.com>
2016-12-23 15:07:39 +0100

    Typo.

--- Oogi/Oogi/Properties/AssemblyInfo.cs
+++ Oogi/Oogi/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Oogi")]
-[assembly: AssemblyDescription("DocumentDB repository pattern ovelord.")]
+[assembly: AssemblyDescription("DocumentDB repository pattern overlord.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Goto10")]
 [assembly: AssemblyProduct("Oogi")]
