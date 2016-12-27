df7f4b098d84110117eb9f6776cd13e29e46afb5
Joey Frohikey <frohikey@gmail.com>
2016-12-27 14:57:59 +0100

    Version 1.1.0.0.

---
+++ Oogi/Oogi/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Oogi
+{
+    public class BaseEntity 
+    {        
+        public virtual string Id { get; set; }     
+        public virtual string Entity { get; set; }        
+    }
+}
