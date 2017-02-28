d4c0dc4963ace7ab408104b91fb406d742e26831
Joey Frohikey <frohikey@gmail.com>
2017-02-28 11:24:40 +0100

    Azure search token added.

---
+++ Oogi/Oogi/Tokens/AzureSearch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Spatial;
+
+namespace Oogi.Tokens
+{
+    public class AzureSearch
+    {
+        public string Index { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Slug { get; set; }
+        public DateTime Date { get; set; }
+        public List<string> Tags { get; set; }
+        public int Number { get; set; }
+        public decimal Money { get; set; }
+        public bool Flag { get; set; }
+        public GeographyPoint Point { get; set; }
+    }
+}
